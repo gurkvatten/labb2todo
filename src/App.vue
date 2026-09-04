c615bd3c3470dbc9bd06603d71ec2b62a705d433
gurkvatten <johanwilhelmkarlsson@gmail.com>
2026-09-04 16:20:39 +0200

    added tasks

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,10 @@
 <script setup>
+import { ref } from 'vue';
+
+const tasks = ref([
+  { id: 1, title: 'Exempeluppgift', project: 'Sidoprojekt A', status: 'todo', priority: 'medium' },
+  { id: 2, title: 'Fixa layout', project: 'Sidoprojekt B', status: 'in-progress', priority: 'high' },
+])
 </script>
 
 <template>
@@ -6,6 +12,8 @@
     <h1>Projects Todo</h1>
   </div>
 </template>
+
+
 
 <style scoped>
 .app {
