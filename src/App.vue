07f200bffc865f6ae7f989459cc5c7ea6fa2f908
gurkvatten <johanwilhelmkarlsson@gmail.com>
2026-09-04 14:44:28 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+</script>
+
+<template>
+  <div class="app">
+    <h1>Projects Todo</h1>
+  </div>
+</template>
+
+<style scoped>
+.app {
+  max-width: 900px;
+  margin: 0 auto;
+  padding: 2rem;
+}
+</style>
